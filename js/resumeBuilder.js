990ea20f3ef781ace02692c13688f7b4e06febd1
patarida <patarida@gmail.com>
2016-05-22 00:48:11 +0700

    Delete Hello world & add bio info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,36 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var skills = ["creative writing", "programming", "HTML/CSS", "Javascript"];
+
+var bio = {
+	"name": "Patarida Kiatsamuttara",
+	"role": "n/a",
+	"contact_info": "Bangkok, Thailand",
+	"pictuer_url": picture_url,
+	"welcome_msg": "Welome to my resume page.",
+	"skills": skills 
+};
+
+for ( bio.skills.length > 0) {
+   $("#header").append(HTMLskillsStart);
+
+   var formattedSkills = HTMLskills.replace("%data%", bio.skills[0]);
+   $("#header").append(formattedSkills);
+
+   var formattedSkills = HTMLskills.replace("%data%", bio.skills[1]);
+   $("#header").append(formattedSkills);
+   
+   var formattedSkills = HTMLskills.replace("%data%", bio.skills[2]);
+   $("#header").append(formattedSkills);
+   
+  var formattedSkills = HTMLskills.replace("%data%", bio.skills[3]);
+   $("#header").append(formattedSkills);
+
+};
+
+var name = bio.name;
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = bio.role;
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
