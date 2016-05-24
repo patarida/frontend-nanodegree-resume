71bd12f33121b1ff197c3d33dc33db14d1e95af8
patarida <patarida@gmail.com>
2016-05-24 14:32:53 +0700

    Add header formattedName and formattedRole

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var name = "Patarida Kiatsamuttara";
+ var formattedName = HTMLheaderName.replace("%data%", name);
+
+ var role = "Web Developer";
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+ 
