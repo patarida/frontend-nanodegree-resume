f2c71f328c6eb0a270f7486ef3eee00979075163
patarida <patarida@gmail.com>
2016-05-25 18:44:04 +0700

    Before add google map and more interactive

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,21 +1,39 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-//---------------------variable arrays-------------------------//
-
+//--------variables declaration and encapsulated display function-------------------------//
 var bio = {
     "name": "Patarida Kiatsamuttara",
-    "role": "Web Developer",
-    "bioPic": "http://postimg.org/image/oerfq3lvf/",
-    "welcome_msg": "Welcome",
-    "skills": ["Python", "HTML/CSS", "JavaScript", "Statistics"],
+    "role": "Petroleum Engineer",
+    "biopic": "images/rig4.jpg",
+    "welcomeMessage": "Welcome to my resume page. It's nice to meet you all.",
+    "skills": ["M/LWD", "Drilling Engineering", "Log Interpretation", "Stuck Pipe Prevention"],
     "contacts": {
         "email": "patarida@gmail.com",
-        "skype": "lhinoroom",
         "mobile": "+66-88-234-8062",
-        "gitHub": "Patarida",
+        "github": "Patarida",
         "twitter": "@happypata",
-        "location": "Bangkok, Thailand",
+        "location": "Bangkok, Thailand"
+    },
+    "display": function() {
+        $("#header").prepend(HTMLheaderRole.replace("%data%", bio["role"]));
+        $("#header").prepend(HTMLheaderName.replace("%data%", bio["name"]));
+        $("#topContacts").append(HTMLmobile.replace("%data%", bio["contacts"]["mobile"]));
+        $("#topContacts").append(HTMLemail.replace("%data%", bio["contacts"]["email"]));
+        $("#topContacts").append(HTMLtwitter.replace("%data%", bio["contacts"]["twitter"]));
+        $("#topContacts").append(HTMLgithub.replace("%data%", bio["contacts"]["github"]));
+        $("#topContacts").append(HTMLlocation.replace("%data%", bio["contacts"]["location"])); 
+        $("#topContacts").append(HTMLbioPic.replace("%data%", bio["biopic"]));
+        $("#topContacts").append(HTMLwelcomeMsg.replace("%data%", bio["welcomeMessage"]));
+        $("#header").append(HTMLskillsStart);
+        bio["skills"].forEach(function(val) {
+            $("#skills").append(HTMLskills.replace("%data%", val));
+        });
+        $("#footerContacts").append(HTMLmobile.replace("%data%", bio["contacts"]["mobile"]));
+        $("#footerContacts").append(HTMLemail.replace("%data%", bio["contacts"]["email"]));
+        $("#footerContacts").append(HTMLtwitter.replace("%data%", bio["contacts"]["twitter"]));
+        $("#footerContacts").append(HTMLgithub.replace("%data%", bio["contacts"]["github"]));
+        $("#footerContacts").append(HTMLlocation.replace("%data%", bio["contacts"]["location"]));
     }
 };
 
@@ -25,21 +43,20 @@ var education = {
         "name": "Chulalongkorn University",
         "location": "Bangkok, Thailand",
         "degree": "Bachelor",
-        "major": "Petroleum Engineer",
+        "majors": "Petroleum Engineer",
         "dates": "2003-2008",
         "url": "http://www.mining.eng.chula.ac.th/",
         },
-           {
+        {
         "name": "Triamudom Suksa School",
         "location": "Bangkok, Thailand",
         "degree": "High School",
-        "major": "Science",
+        "majors": "Science",
         "dates": "2000-2003",
         "url": "http://www.triamudom.ac.th/",
         },
     ],
-
-    "online": [
+    "onlineCourses": [
         {
             "title": "Introduction to programming nanodegree",
             "school": "Udacity",
@@ -58,7 +75,25 @@ var education = {
             "dates": "2016",
             "url": "https://www.codecademy.com/learn/javascript",
         },
-    ]
+    ],
+    "display": function(){
+        education.schools.forEach(function(val) {
+            $("#education").append(HTMLschoolStart);        
+            $(".education-entry:last").append(HTMLschoolName.replace("%data%", val["name"]));
+            $(".education-entry:last").append(HTMLschoolDegree.replace("%data%", val["degree"]));
+            $(".education-entry:last").append(HTMLschoolDates.replace("%data%", val["dates"]));
+            $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", val["location"]));
+            $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", val["majors"]));
+            $(".education-entry:last").append(HTMLschoolURL.replace("%data%", val["url"]));
+        });
+        $(".education-entry:last").append(HTMLonlineClasses);
+        education.onlineCourses.forEach(function(val) {
+            $(".education-entry:last").append(HTMLonlineTitle.replace("%data%", val["title"]));
+            $(".education-entry:last").append(HTMLonlineSchool.replace("%data%", val["school"]));
+            $(".education-entry:last").append(HTMLonlineDates.replace("%data%", val["dates"]));
+            $(".education-entry:last").append(HTMLonlineURL.replace("%data%", val["url"]));
+        });
+    }
 };
 
 var work = {
@@ -68,124 +103,73 @@ var work = {
         "title":"Operation Support Engineer", 
         "location": "Shekou, China", 
         "dates":"2014-2016", 
-        "descriptions":"Technical support for drilling operation."
+        "description":"Technical support for drilling operation."
         },
         {
         "employer":"Schlumberger China", 
         "title":"General Field Engineer", 
         "location": "Tanggu, China", 
         "dates":"2013-2014", 
-        "descriptions":"Lead the drilling & measurement operation projects."
+        "description":"Lead the drilling & measurement operation projects."
         },
         {
         "employer":"Schlumberger Overseas S.A.", 
         "title":"General Field Engineer", 
         "location": "Phitsanulok, Thailand", 
         "dates":"2008-2013", 
-        "descriptions":"Lead the drilling & measurement operation projects."
+        "description":"Lead the drilling & measurement operation projects."
         }
-    ]
+    ],
+    "display": function(){
+        work.jobs.forEach(function(val) {
+            $("#workExperience").append(HTMLworkStart);    
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", val["employer"]);
+            var formattedTitle = HTMLworkTitle.replace("%data%", val["title"]);
+            $(".work-entry:last").append(formattedEmployer + formattedTitle);
+            $(".work-entry:last").append(HTMLworkDates.replace("%data%", val["dates"]));
+            $(".work-entry:last").append(HTMLworkLocation.replace("%data%", val["location"]));
+            $(".work-entry:last").append(HTMLworkDescription.replace("%data%", val["description"]));
+        });
+    }
 };
 
 var projects = {
-    project: [
+    projects: [
         {
         "title": "Rig up operation",
         "dates": "2012",
-        "descriptions": "Rig up operation",
+        "description": "Rig up operation",
         "images": ["images/rig3.jpg"]
         }
-    ]
+    ],
+    "display": function(){
+        projects.projects.forEach(function(val) {
+            $("#projects").append(HTMLprojectStart);    
+            $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", val["title"]));
+            $(".project-entry:last").append(HTMLprojectDates.replace("%data%", val["dates"]));
+            $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", val["description"]));
+            for (i = 0; i < val["images"].length; i++){
+                $(".project-entry:last").append(HTMLprojectImage.replace("%data%", val["images"][i]));
+            }
+        });  
+    }
 };
 //----------end of variable declaratin--------- //
-
-var formattedName = HTMLheaderName.replace("%data%", bio["name"]);
-var formattedRole = HTMLheaderRole.replace("%data%", bio["role"]);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-//------- if skills not empty, add skills------ //
-if (bio["skills"]){
-    $("#header").append(HTMLskillsStart)
-    for (var i in bio["skills"]) {
-        var formattedSkills = HTMLskills.replace("%data%", bio["skills"][i]);
-        $("#skills").append(formattedSkills);
-    };
-};
-
-//--for all jobs in works, append HTMLworkStart for each one-- //
-for (var job in work.jobs) {
-    $("#workExperience").append(HTMLworkStart);    
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job]["title"]);
-    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job]["location"]);
-    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job]["descriptions"]);
-    $(".work-entry:last").append(formattedEmployer + formattedTitle);
-    $(".work-entry:last").append(formattedDates);
-    $(".work-entry:last").append(formattedLocation);
-    $(".work-entry:last").append(formattedDescription);
-};
 
 //---internationalize Button to convert to Firstname LASTNAME style ---//
 function inName(name){   
     names = name.trim().split(" ");
-    return names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase() +" "+names[1].toUpperCase()
-};
+    return names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase() +" "+names[1].toUpperCase();
+}
 // append inName button to main div //
 $("#main").append(internationalizeButton);
 
 
-//------------- add personal data --------------//
+//--call function --//
+bio.display();
+work.display();
+projects.display();
+education.display();
 
-
-
-projects.display = function(){
-    for (var all in projects.project) {
-        $("#projects").append(HTMLprojectStart);    
-        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.project[all]["title"]);
-        var formattedDates = HTMLprojectDates.replace("%data%", projects.project[all]["dates"]);
-        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.project[all]["descriptions"]);
-        $(".project-entry:last").append(formattedTitle);
-        $(".project-entry:last").append(formattedDates);
-        $(".project-entry:last").append(formattedDescription);
-        for (i = 0; i < projects.project[all]["images"].length; i++){
-            var formattedImages = HTMLprojectImage.replace("%data%", projects.project[all]["images"][i]);
-            $(".project-entry:last").append(formattedImages);
-        };
-    };  
-};
-
-education.display = function(){
-    for (var school in education.schools) {
-        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school]["name"]);
-        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school]["degree"]);
-        var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school]["dates"]);
-        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school]["location"]);
-        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school]["major"]);
-        var formattedSchoolURL = HTMLschoolURL.replace("%data%", education.schools[school]["url"]);
-        $("#education").append(HTMLschoolStart);            
-        $(".education-entry:last").append(formattedSchoolName );
-        $(".education-entry:last").append(formattedSchoolDegree );
-        $(".education-entry:last").append(formattedSchoolDates );
-        $(".education-entry:last").append(formattedSchoolLocation );
-        $(".education-entry:last").append(formattedSchoolMajor );
-        $(".education-entry:last").append(formattedSchoolURL );
-    };
-    for (var online in education.online) {        
-        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.online[online]["title"]);
-        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.online[online]["school"]);
-        var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.online[online]["dates"]);
-        var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.online[online]["url"]);
-        
-        $(".education-entry:last").append(formattedOnlineTitle );
-        $(".education-entry:last").append(formattedOnlineSchool );
-        $(".education-entry:last").append(formattedOnlineDates );
-        $(".education-entry:last").append(formattedOnlineURL );
-    };
-};
-
-projects.display()
-education.display()
-
-$("#mapDiv").append(googleMap);
+//--call googleMap --//
+//$("#mapDiv").append(googleMap);
